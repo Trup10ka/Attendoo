8eba8195db0e4a2153600a505d98884dfbab29b6
Lukáš Friedl <lukys3036@gmail.com>
2024-11-14 16:55:09 +0100

    Refactor small part of code

--- attendoo-server/src/main/kotlin/com/trup10ka/attendoo/plugins/Serialization.kt
+++ attendoo-server/src/main/kotlin/com/trup10ka/attendoo/plugins/Serialization.kt
@@ -6,12 +6,15 @@ import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
-fun Application.configureSerialization() {
-    install(ContentNegotiation) {
+fun Application.configureSerialization()
+{
+    install(ContentNegotiation)
+    {
         json()
     }
     routing {
-        get("/json/kotlinx-serialization") {
+        get("/json/kotlinx-serialization")
+        {
             call.respond(mapOf("hello" to "world"))
         }
     }
