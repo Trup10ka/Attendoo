eed63523878d49a19059947e217be77a2dcd1e95
Lukáš Friedl <lukys3036@gmail.com>
2025-02-18 23:01:19 +0100

    Implement request page builder

--- attendoo-client/src/jsMain/kotlin/com/trup10ka/attendoo/pages/builders/RequestPageBuildImp.kt
+++ attendoo-client/src/jsMain/kotlin/com/trup10ka/attendoo/pages/builders/RequestPageBuildImp.kt
@@ -1,16 +1,80 @@
 package com.trup10ka.attendoo.pages.builders
 
 import com.trup10ka.attendoo.data.Request
+import com.trup10ka.attendoo.data.User
+import com.trup10ka.attendoo.util.arrayOf
+import com.trup10ka.attendoo.util.createDiv
+import org.w3c.dom.HTMLElement
+import com.trup10ka.attendoo.pages.constant.StyleClass.*
+import com.trup10ka.attendoo.util.createSpan
+import org.w3c.dom.HTMLDivElement
 
 class RequestPageBuildImp : RequestPageBuilder
 {
-    override fun buildRequestContainer(request: Request)
+    override val currentlyActiveHTMLElements = mutableSetOf<HTMLElement>()
+    
+    override fun buildRequestContainer(appender: HTMLElement?, request: Request)
     {
-        TODO("Not yet implemented")
+        val requestsContainer = createDiv(
+            clazz = arrayOf(INNER_CONTAINER, REQUEST_CONTAINER),
+            child = createDiv(
+                clazz = arrayOf(CONTAINER_TAB, REQUEST),
+                children = arrayOf(
+                    createContainerHeader(request.user),
+                    createInfoContainer(request),
+                    createActionsContainer()
+                )
+            )
+        )
+        
+        appender?.appendChild(requestsContainer)
     }
     
-    override fun eraseRequestContainer()
+    private fun createContainerHeader(user: User): HTMLDivElement
     {
-        TODO("Not yet implemented")
+        return createDiv(
+            clazz = arrayOf(MINI_CONTAINER_HEADER),
+            children = arrayOf(
+                createDiv(
+                    clazz = arrayOf(ONE_LINE_CONTAINER, EMPLOYEE_NAME_CONTAINER),
+                    text = user.fullName
+                ),
+                createDiv(
+                    clazz = arrayOf(ONE_LINE_CONTAINER, AVATAR),
+                    // add avatar somehow
+                )
+            )
+        )
+    }
+    
+    private fun createInfoContainer(request: Request): HTMLDivElement
+    {
+        return createDiv(
+            clazz = arrayOf("request-info"),
+            children = arrayOf(
+                createDiv(clazz = arrayOf(ONE_LINE_CONTAINER), text = request.company),
+                createDiv(clazz = arrayOf(ONE_LINE_CONTAINER), text = request.note)
+            )
+        )
+    }
+    
+    private fun createActionsContainer(): HTMLDivElement
+    {
+        return createDiv(
+            clazz = arrayOf(ACTIONS_CONTAINER),
+            children = arrayOf(
+                createSpan(clazz = arrayOf(MATERIAL_SYMBOLS_OUTLINED, ICON), text = "open_in_full"),
+                createSpan(clazz = arrayOf(MATERIAL_SYMBOLS_OUTLINED, ICON), text = "edit"),
+                createSpan(clazz = arrayOf(MATERIAL_SYMBOLS_OUTLINED, ICON), text = "delete")
+            )
+        )
+    }
+    
+    override fun eraseDynamicElement()
+    {
+        currentlyActiveHTMLElements.forEach {
+            it.remove()
+        }
+        currentlyActiveHTMLElements.clear()
     }
 }
--- attendoo-client/src/jsMain/kotlin/com/trup10ka/attendoo/pages/builders/RequestPageBuilder.kt
+++ attendoo-client/src/jsMain/kotlin/com/trup10ka/attendoo/pages/builders/RequestPageBuilder.kt
@@ -1,9 +1,14 @@
 package com.trup10ka.attendoo.pages.builders
 
 import com.trup10ka.attendoo.data.Request
+import org.w3c.dom.HTMLElement
 
-interface RequestPageBuilder
+interface RequestPageBuilder : PageBuilder
 {
-    fun buildRequestContainer(request: Request)
-    fun eraseRequestContainer()
+    fun buildRequestContainer(appender: HTMLElement? = null, request: Request)
+    
+    override fun buildDynamicElement(appender: HTMLElement?)
+    {
+        throw UnsupportedOperationException("This should not be a reachable part of a program. Contact the developer.")
+    }
 }
