8b734306bed3f9ae80c868a692be7d73b04d7d4b
Trup10ka <lukys3036@gmail.com>
2025-03-02 23:10:36 +0100

    Make KtorHttpClient utilize config properties

--- attendoo-client/src/jsMain/kotlin/com/trup10ka/attendoo/fetch/KtorHttpClient.kt
+++ attendoo-client/src/jsMain/kotlin/com/trup10ka/attendoo/fetch/KtorHttpClient.kt
@@ -1,40 +1,48 @@
 package com.trup10ka.attendoo.fetch
 
+import com.trup10ka.attendoo.data.CLIENT_HOST_VAL
 import io.ktor.client.HttpClient
+import io.ktor.client.call.body
 import io.ktor.client.engine.js.Js
+import io.ktor.client.fetch.Response
 import io.ktor.client.request.request
 import io.ktor.client.request.setBody
 import io.ktor.client.statement.HttpResponse
+import io.ktor.client.statement.bodyAsText
+import io.ktor.http.ContentType
 import io.ktor.http.HttpMethod
+import io.ktor.http.contentType
 import io.ktor.http.headers
 import io.ktor.http.path
 import io.ktor.util.reflect.typeInfo
 import kotlinx.browser.window
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonElement
 
 class KtorHttpClient : com.trup10ka.attendoo.fetch.HttpClient
 {
     private val client = HttpClient(Js)
-
+    
     override suspend fun getVia(path: String): HttpResponse
     {
         return client.request {
             url {
-                host = "localhost"
+                host = CLIENT_HOST_VAL
                 path(path)
             }
             headers {
                 append("Authorization", "Bearer ${window.localStorage.getItem("ATTENDOO_TOKEN")}")
             }
-
+            
             method = HttpMethod.Get
         }
     }
-
+    
     override suspend fun postJSONVia(path: String, body: String): HttpResponse
     {
         return client.request {
             url {
-                host = "localhost"
+                host = CLIENT_HOST_VAL
                 path(path)
             }
             headers {
@@ -50,21 +58,23 @@ class KtorHttpClient : com.trup10ka.attendoo.fetch.HttpClient
         }
     }
     
-    override suspend fun postJSONViaUnauthorized(path: String, body: String): Any
+    override suspend fun postJSONViaUnauthorized(path: String, body: String): JsonElement
     {
-        return client.request {
+        val response = client.request {
             url {
-                host = "localhost"
+                host = CLIENT_HOST_VAL
                 path(path)
             }
             
             method = HttpMethod.Post
-            
+            contentType(ContentType.Application.Json)
             setBody(
                 body,
                 typeInfo<JSON>()
             )
-        }
+        }.bodyAsText()
+        
+        return Json.decodeFromString(response)
     }
     
 }
