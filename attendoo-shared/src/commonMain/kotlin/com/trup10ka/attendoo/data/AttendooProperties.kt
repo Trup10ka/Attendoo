44a7a6a66f41ea4195b05f9b718c62049ff15d75
Trup10ka <lukys3036@gmail.com>
2025-03-02 22:49:39 +0100

    Add properties shared across code needed pre compilated

---
+++ attendoo-shared/src/commonMain/kotlin/com/trup10ka/attendoo/data/AttendooProperties.kt
@@ -0,0 +1,9 @@
+package com.trup10ka.attendoo.data
+
+const val CLIENT_HOST_VAL = "localhost"
+
+/* === Auth Endpoints === */
+const val LOGIN_ENDPOINT = "/api/login"
+
+/* === Storage keys === */
+const val TOKEN_NAME = "ATTENDOO_TOKEN"
