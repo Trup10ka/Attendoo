848312ab94284575cc24cb3bdb4ca06d88d917dc
Trup10ka <lukys3036@gmail.com>
2025-03-16 23:38:01 +0100

    Add request-api to APIRoutes

---
+++ attendoo-server/src/main/kotlin/com/trup10ka/attendoo/api/attendances/Route.kt
@@ -0,0 +1,15 @@
+package com.trup10ka.attendoo.api.attendances
+
+import com.trup10ka.attendoo.db.client.DbClient
+import com.trup10ka.attendoo.db.services.UserAttendanceService
+import com.trup10ka.attendoo.db.services.UserService
+import io.ktor.server.routing.Route
+import io.ktor.server.routing.route
+
+fun Route.routeAttendances(userAttendanceService: UserAttendanceService, userService: UserService)
+{
+    route("/attendances") {
+        routeGetAllAttendances(userAttendanceService, userService)
+        routeGetUserAttendances(userAttendanceService)
+    }
+}
--- attendoo-server/src/main/kotlin/com/trup10ka/attendoo/plugins/routing/APIRoutes.kt
+++ attendoo-server/src/main/kotlin/com/trup10ka/attendoo/plugins/routing/APIRoutes.kt
@@ -1,6 +1,8 @@
 package com.trup10ka.attendoo.plugins.routing
 
+import com.trup10ka.attendoo.api.attendances.routeAttendances
 import com.trup10ka.attendoo.api.auth.routeAuth
+import com.trup10ka.attendoo.api.requests.routeRequests
 import com.trup10ka.attendoo.api.users.routeUsers
 import com.trup10ka.attendoo.db.client.DbClient
 import io.ktor.server.auth.authenticate
@@ -9,5 +11,11 @@ import io.ktor.server.routing.Route
 fun Route.routeAPICalls(dbClient: DbClient)
 {
     routeAuth(dbClient)
-    authenticate { routeUsers(dbClient) }
+    
+    // TODO: Add ATTENDANCES, CREATE_USER, REQUESTS api routes
+    authenticate {
+        routeUsers(dbClient.userService)
+        routeAttendances(dbClient.attendanceService, dbClient.userService)
+        routeRequests(dbClient)
+    }
 }
