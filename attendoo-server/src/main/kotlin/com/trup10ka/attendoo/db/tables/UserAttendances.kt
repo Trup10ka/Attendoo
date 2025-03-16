c334e481bd654b9e36fc5bb40ee83bed8b8cfd00
Trup10ka <lukys3036@gmail.com>
2025-03-16 15:13:28 +0100

    Make user attendance end date nullable

--- attendoo-server/src/main/kotlin/com/trup10ka/attendoo/db/tables/UserAttendances.kt
+++ attendoo-server/src/main/kotlin/com/trup10ka/attendoo/db/tables/UserAttendances.kt
@@ -8,5 +8,5 @@ object UserAttendances : IntIdTable("employee_attendances")
     val employeeId = reference("employee_id", Users)
     val employeeStatusId = reference("employee_status_id", UserStatuses)
     val startDate = date("start_date")
-    val endDate = date("end_date")
+    val endDate = date("end_date").nullable()
 }
