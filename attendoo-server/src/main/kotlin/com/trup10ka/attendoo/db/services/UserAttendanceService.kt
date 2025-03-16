0536ae7b05d9f8c001094c31bb300176b2cb4639
Trup10ka <lukys3036@gmail.com>
2025-03-16 15:56:26 +0100

    Implement Attendance service

---
+++ attendoo-server/src/main/kotlin/com/trup10ka/attendoo/db/services/UserAttendanceService.kt
@@ -0,0 +1,18 @@
+package com.trup10ka.attendoo.db.services
+
+import com.trup10ka.attendoo.db.dao.UserAttendance
+import com.trup10ka.attendoo.dto.UserAttendanceDTO
+import kotlinx.datetime.LocalDate
+
+interface UserAttendanceService
+{
+    suspend fun createAttendance(attendanceDto: UserAttendanceDTO): Boolean
+    suspend fun updateAttendance(attendanceDto: UserAttendanceDTO): Boolean
+    suspend fun deleteAttendance(attendanceDto: UserAttendanceDTO): Boolean
+    suspend fun getAttendanceById(attendanceId: Int): UserAttendance?
+    suspend fun getAttendanceBySpecific(attendanceDto: UserAttendanceDTO): UserAttendance?
+    suspend fun getAttendanceByUserId(userId: Int): List<UserAttendance>
+    suspend fun getAttendanceByUserStatusId(userStatusId: Int): List<UserAttendance>
+    suspend fun getAttendanceByDate(date: LocalDate): List<UserAttendance>
+    suspend fun getAllAttendance(): List<UserAttendance>
+}
