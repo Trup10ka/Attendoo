2d694748cd466d2bfb6ce7be2eb957a2cd9d4190
Lukáš Friedl <lukys3036@gmail.com>
2025-04-07 17:04:38 +0200

    Add debug jwt generation for testing

--- attendoo-server/src/main/kotlin/com/trup10ka/attendoo/Main.kt
+++ attendoo-server/src/main/kotlin/com/trup10ka/attendoo/Main.kt
@@ -1,7 +1,22 @@
 package com.trup10ka.attendoo
 
+import com.auth0.jwt.JWT
+import com.auth0.jwt.algorithms.Algorithm
+import com.trup10ka.attendoo.config.ConfigDistributor.config
+import java.util.Date
+
 fun main()
 {
+    val jwtVerifier = JWT.create()
+        .withAudience(config.jwt.audience)
+        .withIssuer(config.jwt.issuer)
+        .withClaim("attendooUsername", "admin")
+        .withClaim("attendooRole", "admin")
+        .withExpiresAt(Date(System.currentTimeMillis() + 1000000 * 60 * 60 * 24))
+        .sign(Algorithm.HMAC512(config.jwt.secret))
+    
+    println(jwtVerifier)
+    
     val attendoo = Attendoo()
     attendoo.init()
     attendoo.startApp()
