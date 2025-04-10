4da986c4615b57525e6574ed56c49051b79ae60d
Trup10ka <lukys3036@gmail.com>
2025-04-10 19:45:05 +0200

    Implement login page functionality

--- attendoo-client/src/jsMain/kotlin/com/trup10ka/attendoo/pages/LoginPage.kt
+++ attendoo-client/src/jsMain/kotlin/com/trup10ka/attendoo/pages/LoginPage.kt
@@ -1,13 +1,19 @@
 package com.trup10ka.attendoo.pages
 
+import com.trup10ka.attendoo.LOGIN_ENDPOINT
 import com.trup10ka.attendoo.fetch.HttpClient
 import com.trup10ka.attendoo.pages.builders.LoginPageBuilder
 import com.trup10ka.attendoo.pages.constant.PageType
+import com.trup10ka.attendoo.util.getDivByID
 import com.trup10ka.attendoo.util.launchDefaultCoroutine
+import com.trup10ka.attendoo.pages.constant.ElementID.*
+import com.trup10ka.attendoo.util.getButtonByID
+import com.trup10ka.attendoo.util.getInputByID
 import io.ktor.client.call.body
 import io.ktor.client.request.forms.FormDataContent
 import io.ktor.client.statement.HttpResponse
 import io.ktor.http.Parameters
+import kotlinx.serialization.json.Json
 
 class LoginPage(
     override val pageType: PageType,
@@ -18,45 +24,62 @@ class LoginPage(
     
     override fun init()
     {
+        val loginButton = getButtonByID(LOGIN_SUBMIT_BUTTON)!!
+        val usernameInput = getInputByID(USERNAME_INPUT_FIELD)!!
+        val passwordInput = getInputByID(PASSWORD_INPUT_FIELD)!!
+        
+        loginButton.addEventListener("click",  { event ->
+            event.preventDefault()
+            
+            val username = usernameInput.value.trim()
+            val password = passwordInput.value.trim()
+            
+            if (username.isEmpty() || password.isEmpty())
+            {
+                console.log("Username or password is empty")
+                return@addEventListener
+            }
+            launchDefaultCoroutine {
+                performLogin(username, password)
+            }
+        })
     }
-
+    
     override fun show()
     {
+        pageBuilder.buildDynamicElement()
+        init()
     }
-
+    
     override fun hide()
     {
+        pageBuilder.eraseDynamicElement()
     }
-
-    private fun performLogin(username: String, password: String)
+    
+    private suspend fun performLogin(username: String, password: String)
     {
-        launchDefaultCoroutine {
-            val response = httpClient.postJSONViaUnauthorized(
-                "login",
-                parseCredentialsToJSON(username, password)
-            ) as? HttpResponse
-
-            if (response == null)
-            {
-                console.log("Response is null")
-                return@launchDefaultCoroutine
-            }
-
-            val responseText = response.body<String>()
-
-            console.log(responseText)
+        val response = httpClient.postJSONViaUnauthorized(
+            LOGIN_ENDPOINT,
+            parseCredentialsToJSON(username, password)
+        ) as? HttpResponse
+        
+        if (response == null)
+        {
+            console.log("Response is null")
+            return
         }
-
+        
+        val responseText = response.body<String>()
+        
+        console.log(responseText)
     }
-
+    
     private fun parseCredentialsToJSON(username: String, password: String): String
     {
-        return JSON.stringify(
-            FormDataContent(
-                Parameters.build {
-                    append("username", username)
-                    append("password", password)
-                }
+        return Json.encodeToString(
+            mapOf(
+                "username" to username,
+                "password" to password
             )
         )
     }
