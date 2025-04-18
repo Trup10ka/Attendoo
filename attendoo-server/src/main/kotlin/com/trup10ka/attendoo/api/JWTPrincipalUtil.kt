24ff9fc88614c3229acca198f993c47134aa22a4
Trup10ka <lukys3036@gmail.com>
2025-04-18 21:06:21 +0200

    Use defined names for JWT parameters in util properties

--- attendoo-server/src/main/kotlin/com/trup10ka/attendoo/api/JWTPrincipalUtil.kt
+++ attendoo-server/src/main/kotlin/com/trup10ka/attendoo/api/JWTPrincipalUtil.kt
@@ -1,13 +1,16 @@
 package com.trup10ka.attendoo.api
 
+import com.trup10ka.attendoo.JWT_DEPARTMENT_FIELD
+import com.trup10ka.attendoo.JWT_ROLE_FIELD
+import com.trup10ka.attendoo.JWT_USERNAME_FIELD
 import io.ktor.server.auth.jwt.JWTCredential
 import io.ktor.server.auth.jwt.JWTPrincipal
 
 val JWTPrincipal.attendooUsername: String
-    get() = payload.getClaim("attendooUsername").asString()
+    get() = payload.getClaim(JWT_USERNAME_FIELD).asString()
 
 val JWTPrincipal.attendooRole: String
-    get() = payload.getClaim("attendooRole").asString()
+    get() = payload.getClaim(JWT_ROLE_FIELD).asString()
 
 val JWTPrincipal.attendooDepartment: String
-    get() = payload.getClaim("attendooDepartment").asString()
+    get() = payload.getClaim(JWT_DEPARTMENT_FIELD).asString()
