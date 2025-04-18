aa59dec61a2052101813887bd27343f024957e1f
Trup10ka <lukys3036@gmail.com>
2025-04-18 21:33:28 +0200

    Implement users page

--- attendoo-client/src/jsMain/kotlin/com/trup10ka/attendoo/pages/UsersPage.kt
+++ attendoo-client/src/jsMain/kotlin/com/trup10ka/attendoo/pages/UsersPage.kt
@@ -1,9 +1,17 @@
 package com.trup10ka.attendoo.pages
 
 import com.trup10ka.attendoo.AttendooPageManager
+import com.trup10ka.attendoo.FULL_ALL_USERS_ENDPOINT
+import com.trup10ka.attendoo.data.User
 import com.trup10ka.attendoo.fetch.HttpClient
 import com.trup10ka.attendoo.pages.builders.UsersPageBuilderImp
 import com.trup10ka.attendoo.pages.constant.PageType
+import com.trup10ka.attendoo.util.getDivByID
+import com.trup10ka.attendoo.pages.constant.ElementID.MAIN
+import com.trup10ka.attendoo.util.launchDefaultCoroutine
+import io.ktor.client.statement.HttpResponse
+import io.ktor.client.statement.bodyAsText
+import kotlinx.serialization.json.Json
 
 class UsersPage(
     override val pageType: PageType,
@@ -12,19 +20,34 @@ class UsersPage(
 ) : Page
 {
     override val pageBuilder = UsersPageBuilderImp()
-    
+
+    private val users = mutableListOf<User>()
+
     override fun init()
     {
-        TODO("Not yet implemented")
+        launchDefaultCoroutine {
+            val fetchedUsers = httpClient.getVia(FULL_ALL_USERS_ENDPOINT) as HttpResponse
+
+            val responseText = fetchedUsers.bodyAsText()
+            val parsedUsers: List<User> = Json.decodeFromString(responseText)
+            users.clear()
+            users.addAll(parsedUsers)
+            
+            users.forEach { user ->
+                pageBuilder.buildUserContainer(user)
+            }
+        }
     }
-    
+
     override fun show()
     {
-        TODO("Not yet implemented")
+        pageBuilder.buildDynamicElement(getDivByID(MAIN))
+
+        init()
     }
-    
+
     override fun hide()
     {
-        TODO("Not yet implemented")
+        pageBuilder.eraseDynamicElement()
     }
 }
