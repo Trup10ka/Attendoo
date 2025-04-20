4f098b47e10d5ef0410a711ca482abe8cbfa1bfa
Trup10ka <lukys3036@gmail.com>
2025-04-20 18:31:43 +0200

    Implement toDTO for user department

--- attendoo-server/src/main/kotlin/com/trup10ka/attendoo/db/dao/UserDepartment.kt
+++ attendoo-server/src/main/kotlin/com/trup10ka/attendoo/db/dao/UserDepartment.kt
@@ -1,6 +1,7 @@
 package com.trup10ka.attendoo.db.dao
 
 import com.trup10ka.attendoo.db.tables.UserDepartments
+import com.trup10ka.attendoo.dto.DepartmentDTO
 import org.jetbrains.exposed.dao.IntEntity
 import org.jetbrains.exposed.dao.IntEntityClass
 import org.jetbrains.exposed.dao.id.EntityID
@@ -10,4 +11,9 @@ class UserDepartment(id: EntityID<Int>) : IntEntity(id)
     companion object : IntEntityClass<UserDepartment>(UserDepartments)
 
     var name by UserDepartments.name
+    
+    fun toDTO() = DepartmentDTO(
+        id = id.value,
+        name = name
+    )
 }
