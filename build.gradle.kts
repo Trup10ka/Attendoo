1a3e57ae8db5283a7305a4f6ba8eff583e31f11d
Trup10ka <lukys3036@gmail.com>
2025-04-20 13:31:29 +0200

    Implement a gradle task for public distribution

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,14 +20,53 @@ tasks.register<Copy>("buildTestPackage") {
         ":attendoo-client:jsBrowserDevelopmentExecutableDistribution",
         ":attendoo-server:shadowJar"
     )
-    
+
     project(":attendoo-client")
-    
+
     from(project(":attendoo-client").layout.buildDirectory.dir("dist/js/developmentExecutable"))
-    
+
     from(project(":attendoo-server").layout.buildDirectory.dir("libs")) {
         include("*.jar")
     }
-    
+
     into(rootProject.layout.buildDirectory.dir("output"))
+}
+
+
+tasks.register<Copy>("buildPublicDist") {
+    dependsOn(
+        ":kotlinUpgradeYarnLock",
+        ":attendoo-client:jsBrowserDistribution",
+        ":attendoo-server:shadowJar"
+    )
+
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+
+    project(":attendoo-client")
+    
+    from(project(":attendoo-client").layout.buildDirectory.dir("dist/js/productionExecutable")) {
+        include("index.html")
+        into("resources")
+    }
+    
+    from(project(":attendoo-client").layout.buildDirectory.dir("dist/js/productionExecutable")) {
+        include("attendoo-client.js")
+        into("resources/public")
+    }
+    
+    from(project(":attendoo-client").layout.buildDirectory.dir("dist/js/productionExecutable/public")) {
+        into("resources/public")
+    }
+    
+    val versionRegex = Regex("""attendoo-\d+\.\d+\.\d+\.jar""")
+    from(project(":attendoo-server").layout.buildDirectory.dir("libs")) {
+        include("attendoo-*.jar")
+        eachFile {
+            if (!versionRegex.matches(name)) {
+                exclude()
+            }
+        }
+    }
+
+    into(rootProject.layout.projectDirectory.dir("dist"))
 }
