9eb3c75a7624abb3271c17c10e23327664a2d03f
Trup10ka <lukys3036@gmail.com>
2025-04-20 18:31:56 +0200

    Fix get all request route

--- attendoo-server/src/main/kotlin/com/trup10ka/attendoo/api/requests/GetAllRequestsRoute.kt
+++ attendoo-server/src/main/kotlin/com/trup10ka/attendoo/api/requests/GetAllRequestsRoute.kt
@@ -7,6 +7,8 @@ import com.trup10ka.attendoo.data.Request
 import com.trup10ka.attendoo.data.User
 import com.trup10ka.attendoo.db.client.DbClient
 import com.trup10ka.attendoo.db.dbQuery
+import com.trup10ka.attendoo.db.toDTO
+import com.trup10ka.attendoo.util.convertToKotlinxDateTime
 import io.github.oshai.kotlinlogging.KotlinLogging
 import io.ktor.http.HttpStatusCode
 import io.ktor.server.auth.jwt.JWTPrincipal
@@ -22,7 +24,7 @@ fun Route.routeGetAllRequests(dbClient: DbClient)
 {
     get(GET_ALL_REQUESTS_ENDPOINT) {
         logger.info { "Received request for GET_ALL_REQUESTS from ${call.request.origin.remoteHost}:${call.request.origin.remotePort}" }
-        
+
         val principal = call.principal<JWTPrincipal>()
         if (principal == null)
         {
@@ -30,11 +32,48 @@ fun Route.routeGetAllRequests(dbClient: DbClient)
             logger.warn { "Unauthenticated user tried to get all requests from ${call.request.origin.remoteHost}:${call.request.origin.remotePort}" }
             return@get
         }
-        
-        val proposals = try
+
+        val requests = try
         {
             dbQuery {
-                dbClient.proposalService.getAllProposals()
+                val proposals = dbClient.proposalService.getAllProposals()
+                proposals.map { proposal ->
+                    val descriptionParts = proposal.description.split(", Note: ")
+                    val company = if (descriptionParts.isNotEmpty()) descriptionParts[0].removePrefix("Company: ") else ""
+                    val note = if (descriptionParts.size > 1) descriptionParts[1] else ""
+
+                    Request(
+                        proposer = User(
+                            firstName = proposal.proposer.name,
+                            lastName = proposal.proposer.surname,
+                            attendooUsername = proposal.proposer.attendooUsername,
+                            attendooPassword = "",
+                            email = proposal.proposer.email,
+                            phoneNumber = proposal.proposer.phone,
+                            role = proposal.proposer.role.toString(),
+                            userStatus = proposal.proposer.defaultStatus.toString(),
+                            userDepartment = proposal.proposer.department.toString(),
+                            userDepartments = null
+                        ),
+                        proposed = User(
+                            firstName = proposal.proposed.name,
+                            lastName = proposal.proposed.surname,
+                            attendooUsername = proposal.proposed.attendooUsername,
+                            attendooPassword = "",
+                            email = proposal.proposed.email,
+                            phoneNumber = proposal.proposed.phone,
+                            role = proposal.proposed.role.toString(),
+                            userStatus = proposal.proposed.defaultStatus.toString(),
+                            userDepartment = proposal.proposed.department.toString(),
+                            userDepartments = null
+                        ),
+                        proposedDepartment = proposal.proposed.department.name,
+                        note = note,
+                        currentStatus = proposal.currentUserStatus.name,
+                        proposedStatus = proposal.proposedUserStatus.name,
+                        resolvedAt = proposal.resolvedAt?.convertToKotlinxDateTime()
+                    )
+                }
             }
         }
         catch (e: Exception)
@@ -43,41 +82,7 @@ fun Route.routeGetAllRequests(dbClient: DbClient)
             call.respond(HttpStatusCode.InternalServerError, mapOf(ERROR_JSON_FIELD_NAME to "Failed to get requests"))
             return@get
         }
-        
-        val requests = proposals.map { proposal ->
-            val user = dbQuery {
-                dbClient.userService.getUserById(proposal.attendooProposalId)
-            }
-            
-            if (user == null)
-            {
-                logger.warn { "User not found for proposal ${proposal.id.value}" }
-                return@map null
-            }
-            
-            val descriptionParts = proposal.description.split(", Note: ")
-            val company = if (descriptionParts.isNotEmpty()) descriptionParts[0].removePrefix("Company: ") else ""
-            val note = if (descriptionParts.size > 1) descriptionParts[1] else ""
-            
-            Request(
-                user = User(
-                    firstName = user.name,
-                    lastName = user.surname,
-                    attendooUsername = user.attendooUsername,
-                    attendooPassword = "",
-                    email = user.email,
-                    phoneNumber = user.phone,
-                    role = user.role.toString(),
-                    userStatus = user.defaultStatus.toString(),
-                    userDepartment = user.department.toString(),
-                    userDepartments = null
-                ),
-                company = company,
-                note = note,
-                status = proposal.proposedUserStatus.name
-            )
-        }.filterNotNull()
-        
+
         logger.info { "Successfully retrieved ${requests.size} requests" }
         call.respond(HttpStatusCode.OK, requests)
     }
