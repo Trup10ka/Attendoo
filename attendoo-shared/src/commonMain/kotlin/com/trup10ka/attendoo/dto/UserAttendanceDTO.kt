6fc3042fb179082c9fff372fa39fd1fe8f8dba01
Trup10ka <lukys3036@gmail.com>
2025-04-21 01:25:24 +0200

    Make user attendance serializable

--- attendoo-shared/src/commonMain/kotlin/com/trup10ka/attendoo/dto/UserAttendanceDTO.kt
+++ attendoo-shared/src/commonMain/kotlin/com/trup10ka/attendoo/dto/UserAttendanceDTO.kt
@@ -1,7 +1,9 @@
 package com.trup10ka.attendoo.dto
 
 import kotlinx.datetime.LocalDate
+import kotlinx.serialization.Serializable
 
+@Serializable
 data class UserAttendanceDTO(
     val userId: Int,
     val userStatusId: Int,
