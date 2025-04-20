0d9a49f32075f11feee9c42d95a9f171df21ca44
Trup10ka <lukys3036@gmail.com>
2025-04-20 16:46:36 +0200

    Implement requests page

--- attendoo-client/src/jsMain/kotlin/com/trup10ka/attendoo/pages/RequestsPage.kt
+++ attendoo-client/src/jsMain/kotlin/com/trup10ka/attendoo/pages/RequestsPage.kt
@@ -1,9 +1,21 @@
 package com.trup10ka.attendoo.pages
 
 import com.trup10ka.attendoo.AttendooPageManager
+import com.trup10ka.attendoo.FULL_ALL_REQUESTS_ENDPOINT
+import com.trup10ka.attendoo.data.Request
 import com.trup10ka.attendoo.fetch.HttpClient
 import com.trup10ka.attendoo.pages.builders.RequestPageBuildImp
+import com.trup10ka.attendoo.pages.constant.ElementID.MAIN
 import com.trup10ka.attendoo.pages.constant.PageType
+import com.trup10ka.attendoo.pages.constant.StyleClass.CENTER
+import com.trup10ka.attendoo.pages.constant.StyleClass.INNER_CONTAINER
+import com.trup10ka.attendoo.util.getDivByID
+import com.trup10ka.attendoo.util.launchDefaultCoroutine
+import com.trup10ka.attendoo.util.stylesOf
+import com.trup10ka.attendoo.util.createDiv
+import io.ktor.client.statement.HttpResponse
+import io.ktor.client.statement.bodyAsText
+import kotlinx.serialization.json.Json
 
 class RequestsPage(
     override val pageType: PageType,
@@ -12,19 +24,48 @@ class RequestsPage(
 ) : Page
 {
     override val pageBuilder = RequestPageBuildImp()
-    
+
+    private val requests = mutableListOf<Request>()
+
     override fun init()
     {
-        TODO("Not yet implemented")
+        launchDefaultCoroutine {
+            try {
+                val fetchedRequests = httpClient.getVia(FULL_ALL_REQUESTS_ENDPOINT) as HttpResponse
+
+                val responseText = fetchedRequests.bodyAsText()
+                val parsedRequests: List<Request> = Json.decodeFromString(responseText)
+                requests.clear()
+                requests.addAll(parsedRequests)
+
+                val mainContainer = getDivByID(MAIN)
+
+                if (requests.isEmpty()) {
+                    val messageContainer = createDiv(
+                        id = "no-requests-message",
+                        clazz = stylesOf(INNER_CONTAINER, CENTER),
+                        text = "No requests found"
+                    )
+
+                    mainContainer?.appendChild(messageContainer)
+                } else {
+                    requests.forEach { request ->
+                        pageBuilder.buildRequestContainer(mainContainer, request)
+                    }
+                }
+            } catch (e: Exception) {
+                console.error("Error fetching requests: ${e.message}")
+            }
+        }
     }
-    
+
     override fun show()
     {
-        TODO("Not yet implemented")
+        init()
     }
-    
+
     override fun hide()
     {
-        TODO("Not yet implemented")
+        pageBuilder.eraseDynamicElement()
     }
 }
