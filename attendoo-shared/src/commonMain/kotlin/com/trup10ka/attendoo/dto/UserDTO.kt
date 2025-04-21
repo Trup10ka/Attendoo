fd257f28accc71eddc3200f29181f8d5bbb28e0a
Trup10ka <lukys3036@gmail.com>
2025-04-21 18:46:33 +0200

    Add userdto with id

--- attendoo-shared/src/commonMain/kotlin/com/trup10ka/attendoo/dto/UserDTO.kt
+++ attendoo-shared/src/commonMain/kotlin/com/trup10ka/attendoo/dto/UserDTO.kt
@@ -67,3 +67,70 @@ data class UserDTO(
         }
     }
 }
+
+
+@Serializable
+data class UserDTOWithID(
+    val id: Int,
+    val firstName: String? = null,
+    val lastName: String? = null,
+    val attendooUsername: String? = null,
+    val attendooPassword: String? = null,
+    val email: String? = null,
+    val phoneNumber: String? = null,
+    val role: String? = null,
+    val userStatus: String? = null,
+    val userDepartment: String? = null,
+    val userDepartments: List<String>? = null
+)
+{
+    fun checkIfValidForUserCreation(): Boolean
+    {
+        return firstName != null &&
+                lastName != null &&
+                attendooUsername != null &&
+                attendooPassword != null &&
+                email != null &&
+                phoneNumber != null &&
+                role != null &&
+                userStatus != null &&
+                userDepartment != null
+    }
+    
+    fun convertToDTOWithEncryptedPassword(encryptedPassword: String): UserDTO
+    {
+        return UserDTO(
+            this.firstName,
+            this.lastName,
+            this.attendooUsername,
+            encryptedPassword,
+            this.email,
+            this.phoneNumber,
+            this.role,
+            this.userStatus,
+            this.userDepartment,
+            this.userDepartments
+        )
+    }
+    
+    fun isAdmin(): Boolean
+    {
+        return role?.equals("admin", ignoreCase = true) ?: false
+    }
+    
+    fun getAllDepartments(): List<String>
+    {
+        return if (isAdmin() && userDepartments != null)
+        {
+            userDepartments
+        }
+        else if (userDepartment != null)
+        {
+            listOf(userDepartment)
+        }
+        else
+        {
+            emptyList()
+        }
+    }
+}
