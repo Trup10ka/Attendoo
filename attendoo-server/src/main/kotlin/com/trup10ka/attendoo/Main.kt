efaf8565a67721f098ab78edf8d474f6ad6707e5
Trup10ka <lukys3036@gmail.com>
2025-04-21 22:12:22 +0200

    Remove tmp genereting JWT at start

--- attendoo-server/src/main/kotlin/com/trup10ka/attendoo/Main.kt
+++ attendoo-server/src/main/kotlin/com/trup10ka/attendoo/Main.kt
@@ -7,16 +7,6 @@ import java.util.Date
 
 fun main()
 {
-    val jwtVerifier = JWT.create()
-        .withAudience(config.jwt.audience)
-        .withIssuer(config.jwt.issuer)
-        .withClaim("attendooUsername", "admin")
-        .withClaim("attendooRole", "admin")
-        .withExpiresAt(Date(System.currentTimeMillis() + 1000000 * 60 * 60 * 24))
-        .sign(Algorithm.HMAC512(config.jwt.secret))
-    
-    println(jwtVerifier)
-    
     val attendoo = Attendoo()
     attendoo.init()
     attendoo.startApp()
